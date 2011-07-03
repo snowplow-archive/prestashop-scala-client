297fe2ba1878215aeea829083613b2fd7408c2af
Alexander Dean <alexander.dean@keplarllp.com>
2011-07-03 22:03:53 +0300

    Tidied up the comments

--- src/main/scala/co/orderly/prestasac/prestasac.scala
+++ src/main/scala/co/orderly/prestasac/prestasac.scala
@@ -1,6 +1,6 @@
 package co.orderly
 
-/* Distributed as part of prestasac, a xxx.
+/* Distributed as part of prestasac, a Scala client for the PrestaShop Web Service
  *
  * Copyright (c) 2011 Alex Dean
  *
@@ -17,29 +17,12 @@ package co.orderly
  * You should have received a copy of the GNU Affero General Public
  * License along with prestasac. If not, see <http://www.gnu.org/licenses/>.
  */
+
 /**
- * Provides a pair of classes for interacting with the Amazon Product API from Scala.
+ * Provides a class for interacting with PrestaShop Web Service from Scala.
  *
  * ==Overview==
- * The main class to use is [[main.scala.co.orderly.scalapac.scalapac.OperationHelper]]. Create an instance
- * with your Amazon API connection details like so:
- *
- * val opHelper = new OperationHelper(awsAccessKeyId     = "Enter here",
- *                                    awsSecretKey       = "Enter here",
- *                                    awsAssociateTagKey = "Enter here"
- *                                    )
- *
- * Once you have created an opHelper, you can then run operations against the Amazon Product
- * API using the execute command:
- *
- * val (code, xml) = opHelper.execute("ItemSearch", Map("SearchIndex"    -> "Books",
- *                                                      "Keywords"       -> "harry potter",
- *                                                      "ResponseGroup"  -> "ItemAttributes,Offers"
- *                                                     ))
- *
- * For testing queries in the Scala console there is also a helper method, use like so:
- *
- * scala> opHelper.debug("ItemSearch", ...)
+ * TODO
  */
 package object prestasac {
 }
